e1e733f721485b765ed6a6c7d4197490397735ba
Ilya Skapenko <ilyaskapenko@gmail.com>
2025-07-31 00:31:11 +0300

    Minor changes
    * Added nat values to user_balance reply
    * In add_liquidity[_async] added pool creation if it does not exist

--- src/kong_backend/src/user_balances/user_balances_reply.rs
+++ src/kong_backend/src/user_balances/user_balances_reply.rs
@@ -7,6 +7,7 @@ use crate::stable_lp_token::lp_token_map;
 use crate::stable_lp_token::stable_lp_token::StableLPToken;
 use crate::stable_token::stable_token::StableToken;
 use crate::stable_token::token::Token;
+use candid::Nat;
 use crate::stable_token::token_map;
 
 use super::lp_reply::LPReply;
@@ -40,29 +41,31 @@ impl TryFrom<(&StableLPToken, u64)> for UserBalancesReply {
         let pool = token.pool_of().ok_or(())?;
 
         // convert balance to real number
+        let balance_nat = user_lp_token_balance.clone();
         let balance = nat_to_decimals_f64(token.decimals, &user_lp_token_balance).ok_or(())?;
 
         // user_amount_0 = reserve0 * user_lp_token_balance / lp_token_total_supply
         let token_0 = pool.token_0();
         let reserve0 = nat_add(&pool.balance_0, &pool.lp_fee_0);
         let numerator = nat_multiply(&reserve0, &user_lp_token_balance);
-        let raw_amount_0 = nat_divide(&numerator, &lp_token_total_supply).unwrap_or(nat_zero());
-        let amount_0 = nat_to_decimals_f64(token_0.decimals(), &raw_amount_0).ok_or(())?;
-        let usd_amount_0 = ckusdt_amount(&token_0, &raw_amount_0)
-            .and_then(|amount_0| to_ckusdt_decimals_f64(&amount_0).ok_or("Error converting amount 0 to ckUSDT".to_string()))
-            .unwrap_or(0_f64);
+        let amount_0_nat = nat_divide(&numerator, &lp_token_total_supply).unwrap_or(nat_zero());
+        let amount_0 = nat_to_decimals_f64(token_0.decimals(), &amount_0_nat).ok_or(())?;
+        let usd_amount_0_nat = ckusdt_amount(&token_0, &amount_0_nat).unwrap_or(Nat::default());
+        let usd_amount_0 = to_ckusdt_decimals_f64(&usd_amount_0_nat).unwrap_or(0_f64);
 
         // user_amount_1 = reserve1 * user_lp_token_balance / lp_token_total_supply
         let token_1 = pool.token_1();
         let reserve1 = nat_add(&pool.balance_1, &pool.lp_fee_1);
         let numerator = nat_multiply(&reserve1, &user_lp_token_balance);
-        let raw_amount_1 = nat_divide(&numerator, &lp_token_total_supply).unwrap_or(nat_zero());
-        let amount_1 = nat_to_decimals_f64(token_1.decimals(), &raw_amount_1).ok_or(())?;
-        let usd_amount_1 = ckusdt_amount(&token_1, &raw_amount_1)
+        let amount_1_nat = nat_divide(&numerator, &lp_token_total_supply).unwrap_or(nat_zero());
+        let amount_1 = nat_to_decimals_f64(token_1.decimals(), &amount_1_nat).ok_or(())?;
+        let usd_amount_1_nat = ckusdt_amount(&token_1, &amount_1_nat).unwrap_or(Nat::default());
+        let usd_amount_1 = ckusdt_amount(&token_1, &amount_1_nat)
             .and_then(|amount_1| to_ckusdt_decimals_f64(&amount_1).ok_or("Error converting amount 1 to ckUSDT".to_string()))
             .unwrap_or(0_f64);
 
         let usd_balance = usd_amount_0 + usd_amount_1;
+        let usd_balance_nat = usd_amount_0_nat.clone() + usd_amount_1_nat.clone();
 
         Ok(UserBalancesReply::LP(LPReply {
             name: token.name(),
@@ -70,17 +73,23 @@ impl TryFrom<(&StableLPToken, u64)> for UserBalancesReply {
             pool_id: pool.pool_id,
             lp_token_id,
             balance,
+            balance_nat,
             usd_balance,
+            usd_balance_nat,
             chain_0: token_0.chain(),
             symbol_0: token_0.symbol(),
             address_0: token_0.address(),
             amount_0,
+            amount_0_nat,
             usd_amount_0,
+            usd_amount_0_nat,
             chain_1: token_1.chain(),
             symbol_1: token_1.symbol(),
             address_1: token_1.address(),
             amount_1,
+            amount_1_nat,
             usd_amount_1,
+            usd_amount_1_nat,
             ts,
         }))
     }
